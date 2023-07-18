ea7db9bccff53bb2d5b6230aa38cdb0f741d648f
Viswanath Gregarious <41341433+tgrviswanath@users.noreply.github.com>
2023-07-18 09:01:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const { Readable } = require('stream');
 
 const https = require('https');
 
+const fetch = require('node-fetch');
+
  const app = express();
 
 app.use(express.json());
@@ -43,26 +45,59 @@ app.use(express.json());
 
 // });
 
-app.post('/convert', (req, res) => {
+// ---------------2------------------
+// app.post('/convert', (req, res) => {
+// const imageUrl = req.body.url;
+
+// if (!imageUrl) {
+// return res.status(400).json({ error: 'Image URL is required.' });
+// }
+
+// https.get(imageUrl, response => {
+// let data = Buffer.alloc(0);
+// response.on('data', chunk => {
+// data = Buffer.concat([data, chunk]);
+// });
+// response.on('end', () => {
+// res.send({data: data});
+// });
+// }).on('error', error => {
+// res.status(500).json({ error: 'Error fetching the image.' });
+// });
+// });
+// ---------------2------------------
+
+
+---------------3------------------
+app.post('/convert', async (req, res) => {
 const imageUrl = req.body.url;
 
 if (!imageUrl) {
 return res.status(400).json({ error: 'Image URL is required.' });
 }
 
-https.get(imageUrl, response => {
-let data = Buffer.alloc(0);
-response.on('data', chunk => {
-data = Buffer.concat([data, chunk]);
-});
-response.on('end', () => {
-res.send({data: data});
-});
-}).on('error', error => {
-res.status(500).json({ error: 'Error fetching the image.' });
-});
-});
+// https.get(imageUrl, response => {
+// let data = Buffer.alloc(0);
+// response.on('data', chunk => {
+// data = Buffer.concat([data, chunk]);
+// });
+// response.on('end', () => {
+// res.send({data: data});
+// });
+// }).on('error', error => {
+// res.status(500).json({ error: 'Error fetching the image.' });
+// });
+// });
 
+   try {
+    const response = await fetch(imageUrl);
+    const blob = await response.blob();
+ res.send({data: blob});
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error generating form data');
+  }
+---------------3------------------
  app.listen(3000, () => {
 
   console.log('Server is running on port 3000');
