2afdb6e421a43f4d2deee9d550165abc073f917d
Viswanath Gregarious <41341433+tgrviswanath@users.noreply.github.com>
2023-07-18 16:24:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -121,51 +121,101 @@ app.use(express.json());
 // });
  // ------------4------------
 
-// ------------------5---------------
-app.post('/convert', async (req, res) => {
-     let imageUrl = req.body.url;
-      let token = req.body.token;
-     console.log("imageUrl", imageUrl);
-     if (!imageUrl) {
-       return res.status(400).json({ error: 'Image URL is required.' });
-     }
+// // ------------------5---------------
+// app.post('/convert', async (req, res) => {
+//      let imageUrl = req.body.url;
+//       let token = req.body.token;
+//      console.log("imageUrl", imageUrl);
+//      if (!imageUrl) {
+//        return res.status(400).json({ error: 'Image URL is required.' });
+//      }
 
-     try {
-       const response = await fetch(imageUrl);
-       if (!response.ok) {
-         throw new Error('Failed to fetch image');
-       }
+//      try {
+//        const response = await fetch(imageUrl);
+//        if (!response.ok) {
+//          throw new Error('Failed to fetch image');
+//        }
 
-       const buffer = await response.buffer();
-       const base64Data = Buffer.from(buffer).toString('base64');
+//        const buffer = await response.buffer();
+//        const base64Data = Buffer.from(buffer).toString('base64');
 
-       const formData = new FormData();
-       formData.append('screens', Buffer.from(base64Data, 'base64'), 'image.jpg');
-      formData.append('fileType', 'Issue');
+//        const formData = new FormData();
+//        formData.append('screens', Buffer.from(base64Data, 'base64'), 'image.jpg');
+//       formData.append('fileType', 'Issue');
 
 
-       const uploadResponse = await fetch('https://ft-staging-backend.azurewebsites.net/issues/file-check', {
-         method: 'POST',
-          headers:{
-          "x-access-token":token
-          },
-         body: formData,
-       });
+//        const uploadResponse = await fetch('https://ft-staging-backend.azurewebsites.net/issues/file-check', {
+//          method: 'POST',
+//           headers:{
+//           "x-access-token":token
+//           },
+//          body: formData,
+//        });
 
-       if (!uploadResponse.ok) {
-         throw new Error('Failed to upload file');
-       }
-       const uploadResult = await uploadResponse.json();
-console.log("uploadResponse", uploadResult)
+//        if (!uploadResponse.ok) {
+//          throw new Error('Failed to upload file');
+//        }
+//        const uploadResult = await uploadResponse.json();
+// console.log("uploadResponse", uploadResult)
 
-       res.send({data: uploadResult});
-     } catch (error) {
-       console.error(error);
-       res.status(500).send('Error uploading file');
-     }
-   });
+//        res.send({data: uploadResult});
+//      } catch (error) {
+//        console.error(error);
+//        res.status(500).send('Error uploading file');
+//      }
+//    });
 
  // ---------------5-------------
+
+// --------6-------
+app.post('/convert', (req, res) => {
+  const imageUrl = req.body.url; // Assuming imageUrl is sent in the request body
+  const token = req.body.token; // Assuming token is sent in the request body
+
+  const formData = new FormData();
+  fetch(imageUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch image');
+      }
+      return response.arrayBuffer();
+    })
+    .then(buffer => {
+      const file = new Blob([buffer], { type: 'image/jpeg' });
+      formData.append('screens', file, 'image.jpg');
+      formData.append('token', token);
+
+      // Make the API request with the form data
+      fetch('https://ft-staging-backend.azurewebsites.net/issues/file-check', {
+        method: 'POST',
+         headers:{
+                'Content-Type': 'multipart/form-data',
+          "x-access-token":token
+          },
+        body: formData,
+      })
+        .then(uploadResponse => {
+          if (!uploadResponse.ok) {
+            throw new Error('Failed to upload file');
+          }
+          return uploadResponse.json();
+        })
+        .then(uploadResult => {
+           console.log("uploadResult", uploadResult)
+          res.json(uploadResult); // Send the upload result as the response
+        })
+        .catch(error => {
+          console.error(error);
+          res.status(500).json({ error: 'Failed to upload file' }); // Handle the error
+        });
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to fetch image' }); // Handle the error
+    });
+});
+
+// --------6-------
  app.listen(3000, () => {
 
   console.log('Server is running on port 3000');
