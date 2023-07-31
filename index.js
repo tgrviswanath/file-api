8481e7e9d2c58ae566c8804bd6a74792bc3e3e53
Viswanath Gregarious <41341433+tgrviswanath@users.noreply.github.com>
2023-07-31 14:51:19 +0530

    Update index.js uploadArrayImages

--- index.js
+++ index.js
@@ -47,6 +47,50 @@ app.post("/convert", async (req, res) => {
   }
 });
 
+
+app.post("/uploadArrayImages", async (req, res) => {
+  const imageUrls = req.body.urls; // Assuming imageUrl is sent in the request body
+  const token = req.body.token; // Assuming token is sent in the request body
+
+  let data = new FormData();
+  data.append("fileType", "Issue");
+
+  try {
+    for (let i = 0; i < imageUrls.length; i++) {
+
+    const response = await axios.get(imageUrl[i], { responseType: "arraybuffer" });
+    const imageData = response.data;
+    console.log("imageData", imageData);
+
+    data.append("screens", imageData, `image${i}.jpg`);
+    }
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "https://ft-staging-backend.azurewebsites.net/issues/file-check?SID=1689239466",
+      headers: {
+        "x-access-token": token,
+        ...data.getHeaders(),
+      },
+      data: data,
+    };
+
+    axios
+      .request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        res.json({ data: response.data }); // Send the upload result as the response
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({ error: "Failed to upload file" }); // Handle the error
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch image" }); // Handle the error
+  }
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
