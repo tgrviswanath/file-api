c406e5c072b6b19e1443f2107922a1003c6db1aa
Viswanath Gregarious <41341433+tgrviswanath@users.noreply.github.com>
2023-09-13 11:17:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 import axios from "axios";
 import FormData from "form-data";
 import express from "express";
+import decode from "jsqr";
+import Jimp from "jimp";
 
 const app = express();
 
 app.use(express.json());
+
+const keyArray = [
+  "format_version", "creation_date_time", "current_page", "current_page_size",
+  "last_page", "list_size", "checksum", "decoder_serial", "decoder_model",
+  "smartcard_iuc", "load_version", "software_status", "tsid_1", "ts_quality_1",
+  "ts_quality_1", "ts_quality_2", "tsid_3", "ts_quality_3", "tsid_4", "ts_quality_4",
+  "lnb_type", "satellite", "user_band_tuner_1", "user_band_tuner_2",
+  "user_band_tuner_3", "extraview_status", "extraview_primary",
+  "extraview_secondary", "current_channel", "ca_status", "tuner1_ts",
+  "tuner1_quality", "tuner2_ts", "tuner2_quality", "tuner3_ts", "tuner3_quality",
+  "tv_link_power", "rc_modes", "connectivity_status", "connectivity_speed",
+  "connected_services", "remote_recordings", "usage_monitoring", "sec_chip_num",
+  "mac_address", "ca_nationality", "hdd_status", "hdd_available_space",
+  "pg_type", "pg_pin", "channel_blocking"
+];
+
 
 app.post("/convert", async (req, res) => {
   const imageUrl = req.body.url; // Assuming imageUrl is sent in the request body
@@ -97,6 +115,46 @@ console.log("imageUrls typeof ", typeof imageUrls)
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Failed to fetch image" }); // Handle the error
+  }
+});
+
+
+app.post("/processQRCode", async (req, res) => {
+  const imageUrl = req.body.url; 
+  try {
+    // Fetch the QR code image
+    const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+    if (response.status === 200) {
+      const imageData = Buffer.from(response.data, 'binary');
+      Jimp.read(imageData, (err, image) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ error: "Failed to read image" });
+          return;
+        }
+        const width = image.getWidth();
+        const height = image.getHeight();
+        const code = decode(image.bitmap.data, width, height);
+        const values = code.data.split(",");
+        const QRCodeInfo = {};
+    
+        if (values.length === keyArray.length) {
+          for (let i = 0; i < values.length; i++) {
+            QRCodeInfo[keyArray[i]] = values[i];
+          }
+          res.json(QRCodeInfo);
+        } else {
+          console.log("Number of values does not match number of keys.");
+        }
+
+      })
+
+    } else {
+      console.log('Failed to fetch the QR code image.');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch image" });
   }
 });
 
